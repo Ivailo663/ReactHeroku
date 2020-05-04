27f1ef4730baa24a02af091066bc785b4ddf4032
Ivailo663 <ivailo663@gmail.com>
2020-05-04 19:26:20 +0300

    production url

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    prod: "",
+    prod: "https://react-exp-ivaylo.herokuapp.com",
     dev: "http://localhost:3002",
   };
   callingRes = () => {
-    fetch(`${this.state.dev}/asdf`)
+    fetch(`${this.state.prod}/asdf`)
       .then((res) => res.json())
       .then((response) => {
         console.log(response);
