dff7c2531ef46dffe8688cd1f7f892b49d49c112
Georgiev <igeorgiev21@dxc.com>
2020-05-04 14:55:59 +0300

    update template && config class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import Main from "./Main";
-import s from "./App.css";
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1 className={s.header}>Hello from react and express</h1>
-        <Main />
+        <h1>Hello from react</h1>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          query: {
+            presets: ["@babel/react"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
         },
       },
       {
